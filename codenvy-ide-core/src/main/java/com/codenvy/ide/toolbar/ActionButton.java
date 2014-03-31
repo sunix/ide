3fc813122fc006b121d614d61a8937df2cd667ad
Roman Nikitenko <rnikitenko@codenvy.com>
2014-03-31 14:01:27 +0300

    Added debugId for mainMenu, mainToolbar, actions and 'newProjectPage'-menu

--- codenvy-ide-core/src/main/java/com/codenvy/ide/toolbar/ActionButton.java
+++ codenvy-ide-core/src/main/java/com/codenvy/ide/toolbar/ActionButton.java
@@ -76,6 +76,7 @@ public class ActionButton extends Composite implements MouseOverHandler, MouseOu
         setVisible(presentation.isVisible());
         if (presentation.getDescription() != null)
             panel.getElement().setAttribute("title", presentation.getDescription());
+        this.ensureDebugId(place + "/" + actionManager.getId(action));
     }
 
     /** {@inheritDoc} */
